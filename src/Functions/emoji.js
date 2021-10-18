65a00251cb400ac0dfa5d1e77cbeb2a0475d26cf
V1rtuaI <eth.ellis@icloud.com>
2021-10-18 17:25:31 +0100

    Update emoji.js

--- src/Functions/emoji.js
+++ src/Functions/emoji.js
@@ -2,18 +2,18 @@ function removeEmoji(input, includeBasic) {
     if (typeof includeBasic == "undefined")
             includeBasic = true;
 
-        for (var c of input) {
-            var cHex = ("" + c)?.codePointAt(0)?.toString(16) 
-            var lHex = cHex.length;
+        for (const c of input) {
+            const cHex = ("" + c)?.codePointAt(0)?.toString(16);
+            const lHex = cHex.length;
             if (lHex > 3) {
 
-                var prefix = cHex.substring(0, 2);
+                const prefix = cHex.substring(0, 2);
 
-                if (lHex == 5 && prefix == "1f") {
+                if (lHex === 5 && prefix === "1f") {
                     return true;
                 }
 
-                if (includeBasic && lHex == 4) {
+                if (includeBasic && lHex === 4) {
                     if (["20", "21", "23", "24", "25", "26", "27", "2B", "29", "30", "32"].indexOf(prefix) > -1)
                         return true;
                 }
