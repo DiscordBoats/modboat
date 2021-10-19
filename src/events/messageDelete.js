93fc5afde2ab9310d63b69050b1669b7edf65092
Nguyen <68248147+Ne-k@users.noreply.github.com>
2021-10-18 17:39:23 -0700

    Add image and video support

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -5,10 +5,12 @@ module.exports = (client, oldMsg) => {
         return;
     }
 
+
+
     client.channels.fetch(client.config.messagelog).then(channel => {
         const embed = new MessageEmbed()
         .setColor('#dc3b3b')
-        .setDescription(`<@${oldMsg.author.id}> | ${oldMsg.author.tag} (${oldMsg.author.id})\ndeleted a message in <#${oldMsg.channel.id}>\n`).addField('Deleted Message:', `\`${oldMsg.content}\` ${oldMsg.attachments.size > 0 ? '[Attachment]' : ''}`)
+        .setDescription(`<@${oldMsg.author.id}> | ${oldMsg.author.tag} (${oldMsg.author.id})\ndeleted a message in <#${oldMsg.channel.id}>\n`).addField('Deleted Message:', `${oldMsg.content ? oldMsg.content : `[No messages found](${oldMsg.attachments.first() ? oldMsg.attachments.first().proxyURL : null}).`}`).setImage(oldMsg.attachments.first() ? oldMsg.attachments.first().proxyURL : null)
         .setThumbnail(oldMsg.author.avatarURL({ dynamic: true }));
         channel.send(embed);
     });
@@ -18,6 +20,7 @@ module.exports = (client, oldMsg) => {
         content: oldMsg.content,
         author: `${oldMsg.author.tag} (${oldMsg.author.id})`,
         icon: oldMsg.author.avatarURL(),
+        image: oldMsg.attachments.first() ? oldMsg.attachments.first().proxyURL : null,
         timestamp: oldMsg.createdAt
     });
 };
