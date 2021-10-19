8fce8427e277a7cd3dd3a8a63e85b6c863aa5c29
Nguyen <68248147+Ne-k@users.noreply.github.com>
2021-10-18 17:51:32 -0700

    I think this fixed it

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -4,14 +4,9 @@ module.exports = (client, oldMsg, newMsg) => {
         return;
     }
 
-    return client.channels.fetch(client.config.messagelog).then(channel => {
+     client.channels.fetch(client.config.messagelog).then(channel => {
 
-        const embed = new MessageEmbed()
-        .setColor('YELLOW')
-        .setDescription(`<@${oldMsg.author.id}> | ${oldMsg.author.tag} (${oldMsg.author.id})\na [message](https://discord.com/channels/${newMsg.guild.id}/${newMsg.channel.id}/${newMsg.id}) updated in <#${oldMsg.channel.id}>\n`)
-        .addField('Old Message:', `\`${oldMsg.content}\``)
-        .addField("New Message:", `\`${newMsg.content}\``)
-        .setThumbnail(oldMsg.author.avatarURL({ dynamic: true }));
-        channel.send(embed);
+        const embed = new MessageEmbed().setColor('YELLOW').setDescription(`<@${oldMsg.author.id}> | ${oldMsg.author.tag} (${oldMsg.author.id})\na [message](https://discord.com/channels/${newMsg.guild.id}/${newMsg.channel.id}/${newMsg.id}) updated in <#${oldMsg.channel.id}>\n`).addField('Old Message:', `\`${oldMsg.content}\``).addField("New Message:", `\`${newMsg.content}\``).setThumbnail(oldMsg.author.avatarURL({ dynamic: true }));
+        return channel.send(embed);
     });
 };
