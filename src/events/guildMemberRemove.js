fb80902a00aec20c122df0b4f5da7e7d7d632ae1
Nguyen <68248147+Ne-k@users.noreply.github.com>
2021-10-23 16:23:02 -0700

    Added auto ban if user has mute role

--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -4,6 +4,25 @@ module.exports = (client, member) => {
     if (!client.settings.memberlog) {
         return;
     }
+    
+    if(member.roles.cache.find(r => r.name === client.settings.mutedrole)) {
+        client.channels.fetch(client.settings.modlog).then(channel => {
+        const embed = {
+            color: 'dc3b3b',
+            author: {
+                name: 'Auto Ban | Case #<unavailable>',
+                icon_url: member.user.avatarURL({dynamic: true, size: 1024})
+            },
+            description: `**User:** ${member.user.tag} (${banned.user.id})\n**Moderator:** ${client.user.tag} (${client.user.id}\n**Reason:** [Auto Ban] Left the server while having the mute role.`,
+        }
+
+        // Useless timeout
+        setTimeout(async () => {
+            await member.ban({reason: '[Auto Ban] Leaving after being muted.'})
+        }, 0);
+        channel.send({ embed });
+        });
+    }
 
     const unix = Math.floor(new Date(`${member.user.createdAt}`).getTime() / 1000);
     const embed = new MessageEmbed()
