b3ec9690f8082c53f47e87e57e76e38d2cf85345
Jdevdisc <69683068+JDevelo@users.noreply.github.com>
2021-10-24 22:45:05 -0400

    Update dehoist.js

--- src/commands/moderation/dehoist.js
+++ src/commands/moderation/dehoist.js
@@ -7,7 +7,7 @@ module.exports = {
     async execute(client, msg, args) {
         const member = msg.mentions.members.first() || msg.guild.members.cache.get(args[0]);
         if (!member) {
-            return msg.channel.send('Please mention a user to dehoist.');
+            return msg.channel.send('You must say either nickname or username including the user mention.');
         }
 
         if (!member.kickable) {
@@ -20,57 +20,58 @@ module.exports = {
             }
         }
 
-        if (args[0] === 'nickname') {
-            if (member.nickname === null) {
-                return msg.channel.send('This user doesn\'t seem to be hoisting.');
-            }
-
-            if (client.emoji.removeEmoji(member.nickname, false)) {
-                member.edit({ nick: 'No hoisting' });
-                return msg.channel.send(`\`${member.user.tag}\` has been dehoisted.`);
-            }
-
-            const nick = client.config.dehoist.find(s => member.nickname.startsWith(s));
-
-            if (nick) {
-                member.edit({ nick: 'No hoisting' });
-                return msg.channel.send(`\`${member.user.tag}\` has been dehoisted.`);
-            }
-            
-            if (!nick) {
-                return msg.channel.send('This user doesn\'t seem to be hoisting.');
-            }
-
-            if (member.nickname[0] < '0') {
-                member.edit({ nick: 'No hoisting' });
-                return msg.channel.send(`\`${member.user.tag}\` has been dehoisted.`);
-            }
+        switch (args[0]) {
+            case 'nickname':
+                if (member.nickname === null) {
+                    return msg.channel.send('This user doesn\'t seem to be hoisting.');
+                }
+    
+                if (client.emoji.removeEmoji(member.nickname, false)) {
+                    member.edit({ nick: 'No hoisting' });
+                    return msg.channel.send(`\`${member.user.tag}\` has been dehoisted.`);
+                }
+    
+                const nick = client.config.dehoist.find(s => member.nickname.startsWith(s));
+    
+                if (nick) {
+                    member.edit({ nick: 'No hoisting' });
+                    return msg.channel.send(`\`${member.user.tag}\` has been dehoisted.`);
+                }
+                
+                if (!nick) {
+                    return msg.channel.send('This user doesn\'t seem to be hoisting.');
+                }
+    
+                if (member.nickname[0] < '0') {
+                    member.edit({ nick: 'No hoisting' });
+                    return msg.channel.send(`\`${member.user.tag}\` has been dehoisted.`);
+                }
+                break;
+                case 'username':
+                    if (member.user.username === null) {
+                        return msg.channel.send('This user doesn\'t seem to be hoisting.');
+                    }
+        
+                    if (client.emoji.removeEmoji(member.user.username, false)) {
+                        member.edit({ nick: 'No hoisting' });
+                        return msg.channel.send(`\`${member.user.tag}\` has been dehoisted.`);
+                    }
+                    
+                    const user = client.config.dehoist.find(s => member.user.username.startsWith(s));
+                    
+                    if (user) {
+                        member.edit({ nick: 'No hoisting' });
+                        return msg.channel.send(`\`${member.user.tag}\` has been dehoisted.`);
+                    }
+        
+                    if (!user) {
+                        return msg.channel.send('This user doesn\'t seem to be hoisting.');
+                    }
+        
+                    if (member.user.username[0] < '0' ) {
+                        member.edit({ nick: 'No hoisting' });
+                        return msg.channel.send(`\`${member.user.tag}\` has been dehoisted.`);
+                    };
         }
-        if (args[0] === 'username') {
-            if (member.user.username === null) {
-                return msg.channel.send('This user doesn\'t seem to be hoisting.');
-            }
-
-            if (client.emoji.removeEmoji(member.user.username, false)) {
-                member.edit({ nick: 'No hoisting' });
-                return msg.channel.send(`\`${member.user.tag}\` has been dehoisted.`);
-            }
-            
-            const user = client.config.dehoist.find(s => member.user.username.startsWith(s));
-            
-            if (user) {
-                member.edit({ nick: 'No hoisting' });
-                return msg.channel.send(`\`${member.user.tag}\` has been dehoisted.`);
-            }
-
-            if (!user) {
-                return msg.channel.send('This user doesn\'t seem to be hoisting.');
-            }
-
-            if (member.user.username[0] < '0' ) {
-                member.edit({ nick: 'No hoisting' });
-                return msg.channel.send(`\`${member.user.tag}\` has been dehoisted.`);
-            };
-        };
-    }
+    } 
 }
