d5bf349f4b50c4ff1e37373be0e5241c0057765f
Nguyen <68248147+Ne-k@users.noreply.github.com>
2021-11-01 07:01:34 -0700

    Changed to my ID

--- src/Functions/preconditions.js
+++ src/Functions/preconditions.js
@@ -4,7 +4,7 @@ function preconditions(client, msg, command) {
     }
 
     if (command.permissions !== undefined && Array.isArray(command.permissions) && msg.guild) { // Permissions stuff
-        const doesntHave = command.permissions.some(x => !msg.member.permissions.has(x)) || msg.author.id !== '817445802444062790';
+        const doesntHave = command.permissions.some(x => !msg.member.permissions.has(x)) || msg.author.id !== '750510159289254008';
         if (doesntHave) {
             const missing = command.permissions.filter(x => !msg.member.permissions.has(x));
             return msg.channel.send(`You're missing the following permissions: ${missing.join(', ')}`);
