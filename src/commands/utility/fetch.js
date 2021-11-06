ad3f37f8431c879f93c90743642722774b9c503f
Nguyen <68248147+Ne-k@users.noreply.github.com>
2021-11-06 15:34:49 -0700

    ID support for te fetch command

--- src/commands/utility/fetch.js
+++ src/commands/utility/fetch.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js')
 module.exports = {
     name: 'fetch',
     description: 'Find users whose username contains some characters.',
@@ -6,6 +7,25 @@ module.exports = {
     permissions: ['BAN_MEMBERS'],
     async execute(_client, msg, args) {
         const members = filter(await msg.guild.members.fetch(), (_k, v) => v.user.username.toLowerCase().includes(args.join(' ')));
+        if(!args[0]) {
+            let embed2 = new MessageEmbed()
+                .setColor('RANDOM')
+                .setDescription(`Fetch usage example:\n\`\`\`fetch id [User_id]\nfetch [search]\n\`\`\``)
+            return msg.channel.send(embed2);
+        }
+
+        if(args[0] === 'id') {
+            let globaluser =  _client.users.fetch(args[1])
+            if(globaluser) {
+                let gloUserEmbed = new MessageEmbed()
+                    .setTitle('Fetch')
+                    .setDescription(`${(await globaluser).username}#${(await globaluser).discriminator} | ${(await globaluser).id}`)
+                    .addField('Bot:', `${(await globaluser).bot}`)
+                    .setThumbnail((await globaluser).displayAvatarURL({ dynamic: true }))
+                    .setColor('#0099ff')
+                return msg.channel.send(gloUserEmbed)
+            }
+        }
 
         let list = '';
         members.forEach(member => {
