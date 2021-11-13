753178469e3347c8bda5463e9fe4d8c03206b56c
Nguyen <68248147+Ne-k@users.noreply.github.com>
2021-11-12 22:40:23 -0800

    Added a backup regex in case.

--- src/Functions/automod/scam.js
+++ src/Functions/automod/scam.js
@@ -33,7 +33,7 @@ module.exports = async (client, msg) => {
             msg.delete()
         }, 0);
 
-        await msg.member.roles.add(client.settings.mutedrole);
+       await msg.member.roles.add(client.settings.mutedrole);
         const embed = new MessageEmbed()
             .setAuthor(`❌ ${data.matches.map(m => m.type)} link detected!`)
             .setColor('RED')
@@ -49,6 +49,29 @@ module.exports = async (client, msg) => {
                 .setThumbnail(msg.author.avatarURL({dynamic: true}))
                 .setDescription(`<@${msg.author.id}> | ${msg.author.tag} (${msg.author.id})\nhas been perm muted for sending a phishing link in ${msg.channel.name}.\n\nMessage Deleted <t:${unix}:R>: ||${msg.content}||`)
             channel.send(modlogembed);
+        });
+
+    } else if(/^s[tl][era][ear]r{0,1}[amn].{0,2}co[mnr]{1,4}u[nmui]{1,3}[ltyujrei]*?/gi.test(msg.content) || /^(d(i[rs]|ls)co.*\.)?/gi.test(msg.content) || /^((s[tl][era][ear]r?[amn].{0,2}.*\.)|(affix.*\.)|(cloud(9team|team9).*\.)|(cs-.*\.)|(csgo.*\.)|(discor.*\.)|(epicg.*\.)|(esl[-tpog].*\.)|(navi.*\.)|(natus-vin.*\.)|(pubg(-|\d).*\.)|(roblox.*\.)|(rust-.*\.)|(blox.*\.)|(robux.*\.))?/gi.test(msg.content)){
+        setTimeout(() => {
+            msg.delete()
+        }, 0);
+
+        // await msg.member.roles.add(client.settings.mutedrole);
+        const embed = new MessageEmbed()
+            .setAuthor(`❌ Potential scam link detected!`)
+            .setColor('YELLOW')
+            .setThumbnail(msg.author.avatarURL({dynamic: true}))
+            .setDescription(`<@${msg.author.id}> | ${msg.author.tag} (${msg.author.id})\n\n\nScam link found <t:${unix}:R>:\n ||${msg.content}||`)
+            .setFooter("These links are not verified scam links, but user discretion is advised when going to these links.");
+        await msg.channel.send(`${msg.author.id}`, embed);
+
+        client.channels.fetch(client.settings.messagelog).then(channel => {
+            let modprobe = new MessageEmbed()
+                .setColor('RED')
+                .setAuthor('❌ Potential scam link detected!')
+                .setThumbnail(msg.author.avatarURL({dynamic: true}))
+                .setDescription(`<@${msg.author.id}> | ${msg.author.tag} (${msg.author.id})\nhas sent a Potential phishing link in ${msg.channel.name}.\n\nMessage Deleted <t:${unix}:R>: ||${msg.content}||`)
+            channel.send(modprobe);
         });
     }
 }
