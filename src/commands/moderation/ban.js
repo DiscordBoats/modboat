6413e2471edbee280f2af059a257bd2edf1d42b7
cardinn <username.lilrice@gmail.com>
2021-11-25 17:39:10 -0800

    That should fix both ban and kick

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -27,7 +27,10 @@ module.exports = {
             if (ban) {
                 return msg.channel.send('User is already banned');
             }
-
+            await user.send('You have been banned from **' + msg.guild.name + '** for: ' + args.slice(1).join(' '))
+                .catch(() => {
+                    return msg.channel.send('Unable to send ban message to user.');
+                });
             msg.channel.messages.fetch({
                 limit: 100
             }).then((messages) => {
@@ -35,10 +38,7 @@ module.exports = {
                 messages.filter(m => m.author.id === user.user.id).forEach(msg => Messages.push(msg))
                 msg.channel.bulkDelete(Messages)
             })
-            await user.send(`You've been kicked from \`${msg.guild.name}\` for the reason: \`${args.slice(1).join(' ') || 'No reason provided.'}\`.`)
-                .catch(() => {
-                    msg.channel.send(`I was unable to DM \`${user.user.tag}\` to inform them of your kick.`);
-                });
+
             msg.guild.members.ban(user, {
                 reason: `${args.slice(1).join(' ') || 'No reason provided'} [${msg.author.tag}]`
             }).then((banned) => {
