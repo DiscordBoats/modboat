70625ed6ce555e39fb8cd06be614352591b786fc
Nguyen <68248147+Ne-k@users.noreply.github.com>
2021-12-25 18:39:36 -0800

    Fixed anti invite - and updated djs

--- src/Functions/automod/invites.js
+++ src/Functions/automod/invites.js
@@ -1,9 +1,7 @@
-const {MessageEmbed, Permissions} = require('discord.js');
+const {MessageEmbed, Permissions, MessageActionRow, MessageButton} = require('discord.js');
 
 module.exports = async (client, msg) => {
-    if (!client.settings.modrole || !client.settings.mutedrole || !client.settings.messagelog) {
-        return;
-    }
+    const user = msg.guild.members.cache.get(msg.author.id);
 
     const censor = client.automod.invites || [];
     const censorChecks = !!censor.find((word) => {
@@ -16,15 +14,39 @@ module.exports = async (client, msg) => {
     });
 
     if (censorChecks) {
-
+        const balls = new MessageActionRow().addComponents(new MessageButton()
+    .setCustomId('remTime')
+    .setLabel('Remove Timeout')
+    .setStyle('SUCCESS'),
+)
+            .addComponents(new MessageButton()
+                .setCustomId('invite')
+                .setLabel('Invite Info')
+                .setStyle('SECONDARY'),
+            )
 
         try {
+
+            const url = new RegExp(/(^|\s)((https?:\/\/)?[\w-]+(\.[\w-]+)+\.?(:\d+)?(\/\S*)?)/gi)
+            const invite = await client.fetchInvite(msg.content.match(url)[0])
             setTimeout(async () => {
-                    msg.channel.send(`Lmao, looks like <@${msg.author.id}> sent a invite - User Muted.`);
+                await user.timeout(10000 * 60 * 1000, 'Sent a invite link');
+                    msg.channel.send({content: `Lmao, looks like <@${msg.author.id}> sent a invite - User Timed Out.`, components: [balls]});
                     await msg.delete({reason: 'Sent a invite link'})
-                    await msg.member.roles.add(client.settings.mutedrole);
+
 
             }, 1000);
+            client.on('interactionCreate', async (interaction) => {
+                if(interaction.customId === 'remTime') {
+                    if(!interaction.member.permissions.has(Permissions.FLAGS.MODERATE_MEMBERS)) return interaction.reply({content: 'You do not have permission to remove timeouts.', ephemeral: true});
+                    await user.disableCommunicationUntil(null, 'Moderator removed timeout.');
+                    return interaction.reply({content: 'Timeout removed.', ephemeral: true})
+
+                }
+                if(interaction.customId === 'invite') {
+                    interaction.reply({content: `The invite leads to a server called \`${invite.guild.name}\` with a member count of \`${invite.guild.memberCount}\` *${invite.guild.memberCount < 30 ? 'What a lonely server': " "}*`, ephemeral: true})
+                }
+            })
             client.channels.fetch(client.settings.messagelog).then(channel => {
                 const embed = new MessageEmbed()
                     .setColor('RED')
