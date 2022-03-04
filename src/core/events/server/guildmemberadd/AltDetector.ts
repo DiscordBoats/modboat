038a482bf387746b4a550da80d54893dadbe63d8
mrnothingslee-sudo <67931916+mrnothingslee-sudo@users.noreply.github.com>
2022-03-04 18:36:15 -0500

    Update AltDetector.ts

--- src/core/events/server/guildmemberadd/AltDetector.ts
+++ src/core/events/server/guildmemberadd/AltDetector.ts
@@ -14,7 +14,7 @@ export default class AltDetector extends Event {
     async run(member: GuildMember) {
 
 
-   if (member.author.bot) return;
+   if (member.user.bot) return;
         const data = await Schema.findOne({ Guild: member.guild.id });
         if (!data || !data.AltsChannel) {
             return;
