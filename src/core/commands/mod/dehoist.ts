a735e67a1106a428dc0507bef6c0be38adb40081
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-03-10 18:33:16 -0800

    Added normalize

--- src/core/commands/mod/dehoist.ts
+++ src/core/commands/mod/dehoist.ts
@@ -23,11 +23,23 @@ export default class Dehoist extends Command {
         }
 
 
-        await (await message.guild.members.fetch()).forEach(member => {
-            member.setNickname(member.user.username.replace(/[^a-zA-Z0-9\s\w]/g, " ")).catch(e => {
-                return
-            })
-        })
-        message.channel.send("Dehoisted")
+        switch(args[0]) {
+            case "nfd":
+                await (await message.guild.members.fetch()).forEach(member => {
+                    member.setNickname(member.user.username.normalize("NFD").replace(/[\u0300-\u036f]/g, "")).catch(e => {
+                        return
+                    })
+                })
+                message.channel.send("Normalized names")
+                break
+            default:
+                await (await message.guild.members.fetch()).forEach(member => {
+                    member.setNickname(member.user.username.replace(/[^a-zA-Z0-9\s\w]/g, " ")).catch(e => {
+                        return
+                    })
+                })
+                message.channel.send("Dehoisted")
+                break
+        }
     };
 };
