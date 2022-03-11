778a835bb2be485721dbec108cf4409436341ebd
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-03-10 20:22:14 -0800

    Lets see if this does anything

--- src/core/commands/mod/dehoist.ts
+++ src/core/commands/mod/dehoist.ts
@@ -21,21 +21,48 @@ export default class Dehoist extends Command {
             }
             return result;
         }
-        // @ts-ignore
-        const array = []; // eslint-disable-line
 
-        (await message.guild.members.fetch()).forEach(member => {
-            // @ts-ignore
+        switch(args[0]) {
+            case "list":
+                let list = '';
+                (await message.guild.members.fetch()).forEach(member => {
+                    if(/[^a-zA-Z0-9\s\w]/g.test(member.user.username) || /[^a-zA-Z0-9\s\w]/g.test(member.nickname)) {
+                        list += `${member.user.username}#${member.user.discriminator} (${member.user.id}) \`Bot: ${member.user.bot}\`\n`;
+                    }
+                });
 
-            if(/[^a-zA-Z0-9\s\w]/g.test(member.user.username) || /[^a-zA-Z0-9\s\w]/g.test(member.nickname)) {
-                array.push(member.user.id)
-                member.setNickname(member.user.username.replace(/[^a-zA-Z0-9\s\w]/g, " ")).catch(e => {
-                    return
+                if (list === '') {
+                    return message.channel.send('No users found.');
+                }
+
+                return message.channel.send({
+                    embeds: [
+                        new MessageEmbed()
+                            .setTitle("Dehoist List")
+                            .setDescription(list.substring(0, 4096))
+                            .setColor("#00ff00")
+                    ]
                 })
-            }
+                break;
+            default:
+                // @ts-ignore
+                const array = []; // eslint-disable-line
+
+                (await message.guild.members.fetch()).forEach(member => {
+                    // @ts-ignore
+
+                    if(/[^a-zA-Z0-9\s\w]/g.test(member.user.username) || /[^a-zA-Z0-9\s\w]/g.test(member.nickname)) {
+                        array.push(member.user.id)
+                        member.setNickname(member.user.username.replace(/[^a-zA-Z0-9\s\w]/g, " ")).catch(e => {
+                            return
+                        })
+                    }
 
 
-        })
-        return message.channel.send("Dehoisted " + array.length + " member(s)")
+                })
+                return message.channel.send("Dehoisted " + array.length + " member(s)")
+            break
+        }
+
     };
 };
