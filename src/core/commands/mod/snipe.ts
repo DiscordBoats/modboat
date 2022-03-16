864f20a4c7ec4a6ad6f53f544eb3d5952105ec67
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-03-15 17:24:15 -0700

    whoops

--- src/core/commands/mod/snipe.ts
+++ src/core/commands/mod/snipe.ts
@@ -46,24 +46,21 @@ export default class Snipe extends Command {
                 let snipa = this.client.snipe.get(channe)
                 if(!snipa) return message.channel.send("No deleted messages have been found.")
 
-                let unixx = Math.floor(snip.date / 1000)
+                let unixx = Math.floor(snipa.date / 1000)
                 if(snipa.image) {
                     setTimeout(() => {
                         message.channel.send({files: [snipa.image]})
                     }, 1000)
                 }
 
-                return  message.reply({
-                embeds: [
-                    new MessageEmbed()
-                        .setTitle("Snipe")
-                        .setThumbnail(message.guild.iconURL({ dynamic: true }))
-                        //@ts-ignore
-                        .setColor(this.client.color.red)
-                        .setDescription(`Arguments: \`channel [channel]\``)
-
-                ]
-            })
+                return message.channel.send({
+                    embeds: [
+                        new MessageEmbed()
+                            .setColor("#eaecf3")
+                            .setThumbnail(snipa.avatar)
+                            .setDescription(`<@${snipa.user.id}> | ${snipa.user.tag} (${snipa.user.id})\ndeleted the message <t:${unixx}:R> (<t:${unixx}:F>)\n\n\`\`\`${snipa.msg ? snipa.msg : 'No message found maybe a file was attached.'}\`\`\``)
+                    ]
+                })
         }
 
     }
